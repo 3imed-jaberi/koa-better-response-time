6b31ab27c4e1cf151d6f6fb4e1791d9e8d19eafa
3imed-jaberi <imed_jebari@hotmail.fr>
2020-07-13 21:32:50 +0200

    add src code 🚀 ..

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-# init 🚧 ..
+/*!
+ * koa-better-response-time
+ * Copyright(c) 2020 Imed Jaberi
+ * MIT Licensed
+ */
+
+'use strict'
+
+/**
+ * Expose `xResponseTime()`.
+ */
+
+module.exports = xResponseTime
+
+/**
+ * Create a middleware to add a `X-Response-Time` header displaying
+ * the response duration in milliseconds.
+ *
+ * @param {Object} [options]
+ * @param {Number} [options.digits=3]
+ * @param {String} [options.headerName=X-Response-Time]
+ * @param {Boolean} [options.suffix=true]
+ *
+ * @api public
+ */
+
+function xResponseTime (options) {
+  options = options || {}
+
+  // response time digits
+  options.digits = options.digits === 0 ? 0 : options.digits || 3
+  // header name
+  options.headerName = options.headerName || 'X-Response-Time'
+  // display suffix
+  options.suffix = options.suffix === undefined || !!options.suffix
+
+  return async function (ctx, next) {
+    const startAt = process.hrtime()
+    await next()
+    const [seconds, nanoseconds] = process.hrtime(startAt)
+    let time = (seconds * 1e3 + nanoseconds / 1e6).toFixed(options.digits)
+    if (options.suffix) time = `${time}ms`
+
+    ctx.set(options.headerName, time)
+  }
+}
